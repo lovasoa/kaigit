5810821dc14958ae763488e6f7682d6ef9954206
lovasoa <pere.jobs@gmail.com>
2022-05-13 20:25:53 +0200

    add support for all git object types

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,15 +5,17 @@
 use serde::Serialize;
 
 #[derive(Serialize)]
-enum ObjectContents {
+#[serde(tag = "type")]
+enum Object {
   Commit {
     message: String,
     author: String,
     parents: Vec<String>,
-    tree_id: String,
+    tree: String,
   },
   Blob {
     size: usize,
+    short_contents: String,
   },
   Tree {
     children: Vec<TreeEntry>,
@@ -31,20 +33,20 @@ struct TreeEntry {
   id: String,
 }
 
-impl From<git2::Commit<'_>> for ObjectContents {
+impl From<git2::Commit<'_>> for Object {
   fn from(commit: git2::Commit) -> Self {
-    ObjectContents::Commit {
+    Object::Commit {
       message: commit.message().unwrap_or_default().to_string(),
       author: commit.author().name().unwrap_or_default().to_string(),
       parents: commit.parents().map(|p| p.id().to_string()).collect(),
-      tree_id: commit.tree_id().to_string(),
+      tree: commit.tree_id().to_string(),
     }
   }
 }
 
-impl From<git2::Tree<'_>> for ObjectContents {
+impl From<git2::Tree<'_>> for Object {
   fn from(tree: git2::Tree) -> Self {
-    ObjectContents::Tree {
+    Object::Tree {
       children: tree
         .into_iter()
         .map(|entry| TreeEntry {
@@ -56,41 +58,37 @@ impl From<git2::Tree<'_>> for ObjectContents {
   }
 }
 
-impl From<git2::Tag<'_>> for ObjectContents {
+impl From<git2::Tag<'_>> for Object {
   fn from(tag: git2::Tag) -> Self {
-    ObjectContents::Tag {
+    Object::Tag {
       name: tag.name().unwrap_or_default().to_string(),
       target: tag.target_id().to_string(),
     }
   }
 }
 
-impl From<git2::Blob<'_>> for ObjectContents {
+impl From<git2::Blob<'_>> for Object {
   fn from(blob: git2::Blob) -> Self {
-    ObjectContents::Blob {
-      size: blob.content().len(),
+    let content = blob.content();
+    let size = content.len();
+    let short_contents = String::from_utf8_lossy(&content[..size.min(100_000)]).to_string();
+    Object::Blob {
+      size,
+      short_contents,
     }
   }
 }
 
-#[derive(Serialize)]
-struct Object {
-  oid: String,
-  contents: ObjectContents,
-}
-
 impl From<git2::Object<'_>> for Object {
   fn from(obj: git2::Object) -> Self {
-    let oid = obj.id().to_string();
-    let contents = match obj.kind() {
+    match obj.kind() {
       Some(git2::ObjectType::Commit) => obj.into_commit().unwrap().into(),
       Some(git2::ObjectType::Tree) => obj.into_tree().unwrap().into(),
       Some(git2::ObjectType::Blob) => obj.into_blob().unwrap().into(),
       Some(git2::ObjectType::Tag) => obj.into_tag().unwrap().into(),
-      Some(git2::ObjectType::Any) => ObjectContents::Unknown,
-      None => ObjectContents::Unknown,
-    };
-    Object { oid, contents }
+      Some(git2::ObjectType::Any) => Object::Unknown,
+      None => Object::Unknown,
+    }
   }
 }
 
@@ -126,7 +124,7 @@ fn git_data(path: String) -> Result<Repo, String> {
 #[tauri::command]
 fn read_object(path: &str, id: String) -> Result<Object, String> {
   let repo = git2::Repository::open(&path).map_err(tostr)?;
-  let oid :git2::Oid= id.parse().map_err(tostr)?;
+  let oid: git2::Oid = id.parse().map_err(tostr)?;
   let git_obj = repo.find_object(oid, None).map_err(tostr)?;
   Ok(git_obj.into())
 }
